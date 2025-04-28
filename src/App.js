3c917db795d086756db0560166d417708341703f
Erik de Lima Pohlmann <erik.lima@compasso.com.br>
2025-04-28 10:51:23 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Bem-vindo à minha página React!</h1>
+      <p>Essa é a primeira página feita em React.</p>
+    </div>
+  );
+}
+
+export default App;
